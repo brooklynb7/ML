29bc688d15b9f7358a112ae1f7a8de34eb9df7f9
brooklynb7 <brooklynb7@gmail.com>
2018-09-03 21:57:03 +0800

    Update KNN

---
+++ KNN/KNN.py
@@ -0,0 +1,63 @@
+from numpy import *
+import operator
+
+# k-Nearest Neighbors algorithm
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0] # row count
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    # print(sortedDistIndicies)
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    # print(classCount)
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1), reverse=True)
+    # print(sortedClassCount)
+    return sortedClassCount[0][0]
+
+def file2Matrix(filename):
+    love_dictionary={'largeDoses':3, 'smallDoses':2, 'didntLike':1}
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)            #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        if(listFromLine[-1].isdigit()):
+            classLabelVector.append(int(listFromLine[-1]))
+        else:
+            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
+
+def datingClassTest(filename):
+    hoRatio = 0.50      #hold out 10%
+    datingDataMat,datingLabels = file2Matrix(filename)       #load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print('the classifier came back with: {0}, the real answer is: {1}'.format(classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is: {0}".format((errorCount/float(numTestVecs))))
+    print(errorCount)
